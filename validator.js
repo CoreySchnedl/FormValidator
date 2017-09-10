89ceb5531273d51a96a57361a41be9e032e5831e
Corey Schnedl <schnedl.2@osu.edu>
2017-09-09 23:53:09 -0400

    added validator

--- validator.js
+++ validator.js
@@ -1,7 +1,19 @@
 
   //insert your code here
- 
-$(function(){
 
-  //insert your code here
+$(function() {
+
+  $("form[name='sign_in']").validate({
+    rules: {
+      emailInput: {
+        required: true,
+        email: true,
+        minlength: 5
+      },
+      passwordInput: {
+        required: true,
+        minlength: 5
+      }
+    }
+  });
 });
